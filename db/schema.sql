257ff7b958d6afc04d839bdf68632140041fde03
Adam Smith <smith.adam937@gmail.com>
2022-07-21 14:49:20 -0500

    schema.sql tables set up correctly

--- db/schema.sql
+++ db/schema.sql
@@ -14,13 +14,15 @@ CREATE TABLE roles (
   title VARCHAR(30) NOT NULL, 
   department VARCHAR(30) NOT NULL, 
   salary INTEGER NOT NULL,
-  CONSTRAINT fk_deparments FOREIGN KEY (deparments_id) REFERENCES departments(id) ON DELETE SET NULL
+  departments_id INTEGER, 
+  CONSTRAINT fk_deparments FOREIGN KEY (departments_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees ( 
   id INTEGER AUTO_INCREMENT PRIMARY KEY, 
   first_name VARCHAR(30) NOT NULL, 
   last_name VARCHAR(30) NOT NULL,
-  CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL, 
-)
+  roles_id INTEGER, 
+  CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL
+);
 
